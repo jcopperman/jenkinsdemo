ab425cd8da87aa02440765be3c2faa9a2e17b32b
Jonathan Opperman <opperman.jonathan@gmail.com>
2015-12-02 13:36:51 +0200

    second commit

---
+++ JenkinsDemo/JenkinsDemo/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsDemo
+{
+    public class Calculator
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Divide(int divended, int divisor)
+        {
+            return divended / divisor;
+        }
+    }
+}
---
+++ JenkinsDemo/JenkinsDemoTests/JenkinsDemoCalculatorTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JenkinsDemo;
+
+namespace JenkinsDemoTests 
+{
+    [TestClass]
+    public class JenkinsDemoCalculatorTests
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            int x = 1;
+            int y = 2;
+            int expected = 3;
+            int actual = Calculator.Add(x, y);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DivideTest()
+        {
+            int dividend = 10;
+            int divisor = 2;
+            int expected = 5;
+            int actual = Calculator.Divide(dividend, divisor);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.DivideByZeroException))]
+        public void DivideTest_DivisionByZero()
+        {
+            int dividend = 10;
+            int divisor = 0;
+            int actual = Calculator.Divide(dividend, divisor);
+        }
+    }
+}
